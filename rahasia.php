2e14878b61e87e26f07d131055abd81c15be9e76
Kukuh2307 <Kukuhagung12@gmail.com>
2022-09-16 20:08:23 -1200

    memperbaiki url ketika user mencoba masuk tidak melalui halaman login maka akan di redirect ke halaman login

--- rahasia.php
+++ rahasia.php
@@ -1,8 +1,18 @@
 <link rel="stylesheet" href="/css/style.css">
 <?php require './header.php' ?>
-
+<?php 
+// apabila user belum login dan mencoba untuk meredirect ke halaman ini maka akan di arahkan ke halaman login
+if(isset($_SESSION['members_email'])  == ''){
+  header("location:login.php");
+} else {
+?>
 <div class="warning">
   Selamat Datang <?php echo $_SESSION['nama_lengkap']  ?> di webseite kami.
 </div>
+<?php
+} 
+?>
+
+
 
 <?php require './footer.php' ?>
